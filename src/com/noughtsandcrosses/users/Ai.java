4ff2cddaa43e827d06957e6c7831f091fa33deb4
Gregory Romantsov <imoonserk@gmail.com>
2014-03-13 18:52:03 +0400

    some punctuation changes

--- src/com/noughtsandcrosses/users/Ai.java
+++ src/com/noughtsandcrosses/users/Ai.java
@@ -7,14 +7,15 @@ import java.util.Random;
 
 public class Ai extends Users {
 	
-	private Field field = new Field();NN;
+	private Field field = new Field();
 	
 	private char[][] controlField = new char[field.getFieldSize()][field.getFieldSize()];
 	
 	public Ai(char sign){
 		super("Albert", sign);
 	}
-	
+
+
 	//Artifical Intelligence =)
 	
 	@Override
